6a123450e444b79afdebc5041287bd2112c954b0
alperenkurtt <157451917+alperenkurtt@users.noreply.github.com>
2025-01-05 15:02:56 +0300

    Delete Comp301Project/Comp301Project directory

--- Comp301Project/Comp301Project/src/main/java/com/SkyFly/Comp301Project/Exception/FlightNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.SkyFly.Comp301Project.Exception;
-
-public class FlightNotFoundException extends RuntimeException {
-    public FlightNotFoundException(String message) {
-        super(message);
-    }
-}
--- Comp301Project/Comp301Project/src/main/java/com/SkyFly/Comp301Project/Repository/FlightRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SkyFly.Comp301Project.Repository;
-
-import com.SkyFly.Comp301Project.Model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-
-}
